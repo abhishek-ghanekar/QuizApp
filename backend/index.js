c1df6e3cad804de3b60fec97280b4c945c89c598
Abhishek Ghanekar <a8hiyt@gmail.com>
2024-07-03 21:56:01 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = "3000"
-
+const port = 3000
 app.use(cors())
 app.use(express.json())//if we want to use req.body this middleware is required
 
+app.get('/',(req,res) => {
+  res.send('Hello from backend')
+})
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/quiz', require('./routes/quiz'))
